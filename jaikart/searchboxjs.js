e08b55ad3eec5d8c5e78b7013c12b64dd0d687df
JaideepSai567 <122475642+JaideepSai567@users.noreply.github.com>
2023-01-11 23:07:05 +0530

    Add files via upload

---
+++ jaikart/searchboxjs.js
@@ -0,0 +1,16 @@
+ function search_icon_func(){
+   var a= document.getElementById("search-bar").value;
+   if(a=="Zaveri Pearls" || a== "Zaveri Pearls Jewellery for Women"){
+      window.open("jew1html.html");
+      
+   }
+   if(a=="Mansiyaorange" || a== "Mansiyaorange Jewellery for Women"){
+      window.open("jew3html.html");
+   }
+   if(a=="Shining Diva Fashion" || a== "Shining Diva Fashion Jewellery for Women"){
+      window.open("jew4html.html");
+   }
+   if(a=="Peora Jewellery for Women" || a== "Peora"){
+      window.open("jew6html.html");
+   }
+  }
